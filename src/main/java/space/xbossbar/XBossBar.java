6c6c9d2632126c738a2bfcecb082ab5c36a85cf5
admin <net.nullx@gmail.com>
2024-03-13 17:55:33 -0300

    Initial commit

---
+++ src/main/java/space/xbossbar/XBossBar.java
@@ -0,0 +1,17 @@
+package space.xbossbar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XBossBar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
